9b84bff0b0d40b3d1961dc3c990dcfdd54e30c93
Martin Flischman <26260699+martinflischman@users.noreply.github.com>
2026-08-11 12:34:08 +0200

    feat: render todo cards and list
    - implement TodoCard UI to display todo content and action buttons
    - wire TodoList to render and filter todos from props

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,21 @@
 import { TodoCard } from "./TodoCard";
 
-export function TodoList() {
-  return <div></div>;
+export function TodoList(props) {
+  const { todos } = props;
+
+  const tab = "All";
+  const filterTodosList =
+    tab === "All"
+      ? todos
+      : tab === "Completed"
+        ? todos.filter((val) => val.complete)
+        : todos.filter((val) => !val.complete);
+
+  return (
+    <>
+      {filterTodosList.map((todo, todoIndex) => {
+        return <TodoCard key={todoIndex} todo={todo} />;
+      })}
+    </>
+  );
 }
